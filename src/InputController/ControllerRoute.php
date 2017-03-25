efa3f40495fe78a63e0e45ca1f454886e324f1cd
Loenix <florent@sowapps.com>
2017-03-25 23:15:11 +0100

    Routing is now able to include and require some package configs

--- src/InputController/ControllerRoute.php
+++ src/InputController/ControllerRoute.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ControllerRoute
- */
+*/
 
 namespace Orpheus\InputController;
 
@@ -14,64 +14,64 @@ use Orpheus\Core\RequestHandler;
 
 /**
  * The ControllerRoute class
- * 
+ *
  * @author Florent Hazard <contact@sowapps.com>
  *
  */
 abstract class ControllerRoute extends Route {
-	
+
 	/**
 	 * A route is identified by its name
-	 * 
+	 *
 	 * @var string The name
 	 */
 	protected $name;
-	
+
 	/**
 	 * The path determine how to access this route
-	 * 
+	 *
 	 * @var string The path
 	 */
 	protected $path;
-	
+
 	/**
 	 * The class of called controller associated to this route
-	 * 
+	 *
 	 * @var string The controller class
 	 */
 	protected $controllerClass;
-	
+
 	/**
 	 * Non-processed options in route configuration
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $options;
-	
+
 	/**
 	 * Restrictions to access this route
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $restrictTo;
-	
+
 	/**
 	 * Default response if controller returns is invalid
-	 * 
+	 *
 	 * @var OutputResponse
 	 */
 	protected $defaultResponse;
-	
+
 	/**
 	 * Registered routes
 	 *
 	 * @var array
 	 */
 	protected static $routes = array();
-	
+
 	/**
 	 * Registered route restrictions
-	 * 
+	 *
 	 * @var array
 	 */
 	protected static $routesRestrictions = array();
@@ -94,31 +94,31 @@ abstract class ControllerRoute extends Route {
 		$this->options		= $options;
 		$this->defaultResponse	= $defaultResponse;
 	}
-	
+
 	/**
 	 * Test if the route matches the given $request
-	 * 
+	 *
 	 * @param \Orpheus\InputController\InputRequest $request
 	 * @param array $values
 	 * @param boolean $alternative True if we are looking for an alternative route, because we did not find any primary one
 	 */
 	public abstract function isMatchingRequest(InputRequest $request, &$values=array(), $alternative=false);
-	
+
 	/**
 	 * Get all registered routes
 	 * Routes are commonly stored in the configuration
-	 * 
+	 *
 	 * @return \Orpheus\InputController\ControllerRoute[]
 	 */
 	public static function getRoutes() {
 		static::initialize();
 		return static::$routes;
-// 		throw new Exception('The class "'.get_called_class().'" should override the `getRoutes()` static method from "'.get_class().'"');
+		// 		throw new Exception('The class "'.get_called_class().'" should override the `getRoutes()` static method from "'.get_class().'"');
 	}
-	
+
 	/**
 	 * Register a route configuration
-	 * 
+	 *
 	 * @param string $name
 	 * @param array $config
 	 * @throws \Exception
@@ -126,10 +126,10 @@ abstract class ControllerRoute extends Route {
 	public static function registerConfig($name, array $config) {
 		throw new \Exception('The class "'.get_called_class().'" should override the `registerConfig()` static method from "'.get_class().'"');
 	}
-	
+
 	/**
 	 * Run the $request by processing the matching controller
-	 * 
+	 *
 	 * @param \Orpheus\InputController\InputRequest $request
 	 * @throws NotFoundException
 	 * @throws ForbiddenException
@@ -142,29 +142,24 @@ abstract class ControllerRoute extends Route {
 				throw new NotFoundException('The controller "'.$this->controllerClass.'" was not found');
 			}
 			$request->setRoute($this);
-			
+				
 			//Wow, we made it to handle session, ok ?
 			$this->prepare($request);
-			
+				
 			if( !$this->isAccessible() ) {
 				throw new ForbiddenException('This route is not accessible in this context');
 			}
 			$controller = $this->instanciateController();
-// 			$controllerClass = $this->controllerClass;
-// 			$controller = new $controllerClass();
-// 			if( !($controller instanceof Controller) ) {
-// 				throw new NotFoundException('The controller "'.$this->controllerClass.'" is not a valid controller, the class must inherit from "'.get_class().'"');
-// 			}
 			$result	= $controller->process($request);
 			return $result;
 		} catch( Exception $exception ) {
 			return $this->processException($exception);
 		}
 	}
-	
+
 	/**
 	 * Process the given $exception with the default response
-	 * 
+	 *
 	 * @param \Orpheus\Exception\UserException $exception
 	 * @param array $values
 	 * @return \Orpheus\InputController\OutputResponse
@@ -187,28 +182,28 @@ abstract class ControllerRoute extends Route {
 		$response = $this->defaultResponse;
 		return $response::generateFromException($exception);
 	}
-	
+
 	/**
 	 * Define this class initialized
-	 * 
+	 *
 	 * @var string
 	 */
 	protected static $initialized = false;
-	
+
 	/**
 	 * Initialize the route class by loading the configuration (once only)
 	 */
 	public static function initialize() {
-// 		debug('ControllerRoute::initialize()');
+		// 		debug('ControllerRoute::initialize()');
 		if( static::isInitialized() ) { return; }
 		static::$initialized = true;
-		
+
 		$routes = array();
-		
+
 		// TODO: Add cache, but what kind of ?
-		
+
 		static::loadRoutes($routes);
-		
+
 		// Register routes
 		foreach( $routes as $type => $typeRoutes ) {
 			$routeClass = RequestHandler::getRouteClass($type);
@@ -220,11 +215,18 @@ abstract class ControllerRoute extends Route {
 			}
 		}
 	}
-	
+
 	const REQUIREMENTS_KEY = 'require-packages';
-	
+
+	/**
+	 * Load routes from $package or app (if null)
+	 *
+	 * @param array $routes
+	 * @param string $package
+	 */
 	protected static function loadRoutes(&$routes, $package=null) {
-		
+		// TODO: Protect against loop
+
 		$packageRoutes = array();
 		// Load prod routes (all environments routes)
 		static::populateRoutesFromFile($packageRoutes, 'routes', $package);
@@ -235,10 +237,9 @@ abstract class ControllerRoute extends Route {
 				// Optional
 				static::populateRoutesFromFile($packageRoutes, 'routes_dev', $package);
 			} catch( \Exception $e ) {
-				
 			}
 		}
-		
+
 		if( !empty($packageRoutes[self::REQUIREMENTS_KEY]) && is_array($packageRoutes[self::REQUIREMENTS_KEY]) ) {
 			$packageAndRequiredRoutes = array();
 			foreach( $packageRoutes[self::REQUIREMENTS_KEY] as $requirement ) {
@@ -255,73 +256,86 @@ abstract class ControllerRoute extends Route {
 			unset($packageRoutes[self::REQUIREMENTS_KEY]);
 			static::mergeRoutes($packageAndRequiredRoutes, $packageRoutes);
 			unset($packageRoutes);
-			
+				
 		} else {
-			
+				
 			// Remove invalid requirements
 			unset($packageRoutes[self::REQUIREMENTS_KEY]);
 			$packageAndRequiredRoutes = &$packageRoutes;
-			
+				
 		}
-		
+
 		static::mergeRoutes($routes, $packageAndRequiredRoutes);
 	}
-	
+
+	/**
+	 * Populate given array with routes from $file in $package (or app if null)
+	 *
+	 * @param array $routes
+	 * @param string $file
+	 * @param string $package
+	 */
 	protected static function populateRoutesFromFile(&$routes, $file, $package=null) {
 		$conf = YAML::buildFrom($package, $file, true);
 		static::mergeRoutes($routes, $conf->asArray());
 	}
-	
+
+	/**
+	 * Merge routes array with routes' logic
+	 *
+	 * @param array $routes
+	 * @param array $added
+	 */
 	protected static function mergeRoutes(&$routes, $added) {
 		// First level (only) is merged
 		foreach( $added as $type => $typeRoutes ) {
 			if( isset($routes[$type]) ) {
-				$routes[$type]	= array_merge($typeRoutes, $routes[$type]);
+				$routes[$type] = array_merge($typeRoutes, $routes[$type]);
 			} else {
-				$routes[$type]	= $typeRoutes;
+				$routes[$type] = $typeRoutes;
 			}
 		}
 	}
-	
+
 	/**
 	 * Test if the class is initialized
-	 * 
+	 *
 	 * @return boolean
 	 */
 	public static function isInitialized() {
 		return static::$initialized;
 	}
-	
+
 	/**
 	 * Get the name
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getName() {
 		return $this->name;
 	}
-	
+
 	/**
 	 * Get the path
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getPath() {
 		return $this->path;
 	}
-	
+
 	/**
 	 * Get the controller class
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getControllerClass() {
 		return $this->controllerClass;
 	}
-	
+
 	/**
 	 * Instanciate the controller and return it
-	 * 
+	 *
 	 * @return \Orpheus\InputController\Controller
 	 */
 	public function instanciateController() {
@@ -334,18 +348,18 @@ abstract class ControllerRoute extends Route {
 		$controller->setRoute($this);
 		return $controller;
 	}
-	
+
 	/**
 	 * Get route options
-	 * 
+	 *
 	 * @return array
 	 */
 	public function getOptions() {
 		return $this->options;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * {@inheritDoc}
 	 * @see \Orpheus\Core\Route::isAccessible()
 	 */
@@ -365,27 +379,27 @@ abstract class ControllerRoute extends Route {
 		}
 		return true;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * {@inheritDoc}
 	 * @see \Orpheus\Core\Route::getLink()
 	 */
 	public function getLink() {
 		return $this->formatURL();
 	}
-	
+
 	/**
 	 * Format the URL to this route using $values
-	 * 
+	 *
 	 * @param array $values
 	 */
 	public abstract function formatURL($values=array());
-	
+
 	/**
 	 * Register the access restriction $type
 	 * This will be used by isAccessible()
-	 * 
+	 *
 	 * @param string $type
 	 * @param callable $callable
 	 * @uses isAccessible()
@@ -393,15 +407,15 @@ abstract class ControllerRoute extends Route {
 	public static function registerAccessRestriction($type, $callable) {
 		static::$routesRestrictions[$type] = $callable;
 	}
-	
+
 	/**
 	 * Get the current route name
-	 * 
+	 *
 	 * @return string
 	 */
 	public static function getCurrentRouteName() {
 		return InputRequest::getMainRequest()->getRouteName();
 	}
-	
-	
+
+
 }
